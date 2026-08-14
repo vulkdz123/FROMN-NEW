3454394f04d85b5bda2e6996da804fb084cf567a
vulkdz123 <tangtruongvu237@gmail.com>
2026-08-14 17:53:48 +0700

    Update layout.tsx

--- src/app/Ads-Policy-review/layout.tsx
+++ src/app/Ads-Policy-review/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-    title: 'Page Ads-Policy Review',
+    title: 'Review Page',
 };
 
 export default function AccountQualityStatusLayout({
